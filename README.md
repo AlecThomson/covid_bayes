da21e041487b3d9d977ff388532a7d08a0b43d55
Thomson, Alec (S&A, Kensington) <alec.thomson@csiro.au>
2022-07-15 14:41:36 +0800

    Docs

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # covid bayes
 
+```
+           _._
+        .-'   `
+      __|__
+     /     \
+     |()_()|
+     \{o o}/
+      =\o/=
+       ^ ^
+```
+
 A simple little program for calculating Bayesian likelihoods of tests like RATs
 
 This is for 'fun'
--- rat_bayes.py
+++ rat_bayes.py
@@ -44,4 +44,21 @@ def main():
         main()
 
 if __name__ == "__main__":
+    desc_str = r"""
+           _._
+        .-'   `
+      __|__
+     /     \
+     |()_()|
+     \{o o}/
+      =\o/=
+       ^ ^
+
+A simple little program for calculating Bayesian likelihoods of tests like RATs
+
+This is for 'fun'
+
+!!! Please don't use this as health advice !!!
+    """
+    print(desc_str)
     main()
